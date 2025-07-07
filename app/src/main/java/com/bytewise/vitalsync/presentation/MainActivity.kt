ea68b5bf88dd3ac350bcd2932365d24680cd8708
Jay Kerkar <bytewise0405@gmail.com>
2025-07-07 21:12:23 +0530

    Refactor MainActivity to use VitalSyncApp and handle network states with LoadingScreen and NoNetworkScreen

--- app/src/main/java/com/bytewise/vitalsync/presentation/MainActivity.kt
+++ app/src/main/java/com/bytewise/vitalsync/presentation/MainActivity.kt
@@ -1,70 +1,72 @@
-/* While this template provides a good starting point for using Wear Compose, you can always
- * take a look at https://github.com/android/wear-os-samples/tree/main/ComposeStarter to find the
- * most up to date changes to the libraries and their usages.
- */
-
 package com.bytewise.vitalsync.presentation
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.wear.compose.material.MaterialTheme
-import androidx.wear.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
+import androidx.activity.ComponentActivity
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.runtime.collectAsState
 import androidx.wear.compose.material.TimeText
-import androidx.wear.tooling.preview.devices.WearDevices
-import com.bytewise.vitalsync.R
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.bytewise.vitalsync.presentation.screens.HomeScreen
+import com.bytewise.vitalsync.presentation.models.NetworkState
 import com.bytewise.vitalsync.presentation.theme.VitalSyncTheme
+import com.bytewise.vitalsync.presentation.utils.NetworkManager
+import com.bytewise.vitalsync.presentation.screens.LoadingScreen
+import com.bytewise.vitalsync.presentation.screens.NoNetworkScreen
+import com.bytewise.vitalsync.presentation.viewmodels.NetworkViewModel
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
-
         super.onCreate(savedInstanceState)
-
         setTheme(android.R.style.Theme_DeviceDefault)
 
+        val networkManager = NetworkManager(this)
+
         setContent {
-            WearApp("Android")
+            VitalSyncApp(networkManager = networkManager)
         }
     }
 }
 
 @Composable
-fun WearApp(greetingName: String) {
+fun VitalSyncApp(networkManager: NetworkManager) {
+    val networkViewModel: NetworkViewModel = viewModel { NetworkViewModel(networkManager) }
+    val networkState by networkViewModel.networkState.collectAsState()
+
     VitalSyncTheme {
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .background(MaterialTheme.colors.background),
-            contentAlignment = Alignment.Center
+                .background(Color.Black)
         ) {
             TimeText()
-            Greeting(greetingName = greetingName)
+
+            when (networkState) {
+                is NetworkState.Loading -> {
+                    LoadingScreen()
+                }
+
+                is NetworkState.Connected -> {
+                    if ((networkState as NetworkState.Connected).hasInternet) {
+                        HomeScreen()
+                    } else {
+                        NoNetworkScreen(
+                            onRetry = { networkViewModel.retryConnection() })
+                    }
+                }
+
+                is NetworkState.Disconnected -> {
+                    NoNetworkScreen(
+                        onRetry = { networkViewModel.retryConnection() })
+                }
+            }
         }
     }
-}
-
-@Composable
-fun Greeting(greetingName: String) {
-    Text(
-        modifier = Modifier.fillMaxWidth(),
-        color = MaterialTheme.colors.primary,
-        text = stringResource(R.string.hello_world, greetingName)
-    )
-}
-
-@Preview(device = WearDevices.SMALL_ROUND, showSystemUi = false)
-@Composable
-fun DefaultPreview() {
-    WearApp("Preview Android")
 }
